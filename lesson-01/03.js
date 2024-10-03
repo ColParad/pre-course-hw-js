df67d286a345635994da0ddf58a95a2f8f4599c8
ColParad <info@all4coins.ru>
2024-10-03 22:30:18 +0300

    Lesson 1 part 3

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,3 +1,3 @@
 let number = 1;
 
-console.log(number++ + "," + ++number + "," + number-- + "," + --number );
+console.log(number++ + ","); console.log(++number + ","); console.log(number-- + ","); console.log(--number )
